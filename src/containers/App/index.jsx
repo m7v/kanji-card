68ad7ce9eb76974741808c3fcff26039d9419af2
Михайлов Андрей Андреевич <a.mikhaylov@2gis.ru>
2019-07-01 14:16:41 +0700

    Sorted array.

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import orderBy from 'lodash/orderBy';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import kanjiList from './kanjiList.json';
 import { Swipe } from '../../components/Swipe';
 // import KanjiCard from '../../components/KanjiCard';
 
-const cards = Object.values(kanjiList.items);
+const cards = orderBy(Object.values(kanjiList.items), (o) => o.tags[0].match(/\d/g)[0], 'desc');
 
 export default function App() {
   const classes = useStyles();
