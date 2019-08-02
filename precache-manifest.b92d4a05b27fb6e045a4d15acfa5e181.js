56906b8851184d616735fcdf876db2167367f7a0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-02 04:40:02 +0000

    Deploy m7v/kanji-card to github.com/m7v/kanji-card.git:gh-pages

---
+++ precache-manifest.b92d4a05b27fb6e045a4d15acfa5e181.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d062aa60a66c7229e2ba07c1045a15",
+    "url": "/kanji-card/index.html"
+  },
+  {
+    "revision": "6f11a7bdc62af23046bb",
+    "url": "/kanji-card/static/css/main.5462bd46.chunk.css"
+  },
+  {
+    "revision": "441785204f8400ecbec7",
+    "url": "/kanji-card/static/js/2.901cbdf5.chunk.js"
+  },
+  {
+    "revision": "6f11a7bdc62af23046bb",
+    "url": "/kanji-card/static/js/main.75aadb6d.chunk.js"
+  },
+  {
+    "revision": "c759512aa76a34df7ad2",
+    "url": "/kanji-card/static/js/runtime~main.c753acb8.js"
+  }
+]);
